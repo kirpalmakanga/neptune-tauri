5c35bb06910cb28e18ca5b254270756e5d7f1fd2
Kirpal <makanga.kirpal@gmail.com>
2024-10-06 19:01:49 +0200

    removed icon sprite

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,12 @@
 import { onMount, ParentComponent } from 'solid-js';
 import { useNavigate } from '@solidjs/router';
 import { Transition } from 'solid-transition-group';
-import Sprite from './components/Sprite';
+import Menu from './components/Menu';
 // import Prompt from './components/Prompt';
 import Playlists from './components/Playlists';
 import Player from './components/player/Player';
 import Notifications from './components/Notifications';
 import { usePlaylists } from './store/playlists';
-import Menu from './components/Menu';
 
 const Root: ParentComponent = (props) => {
     const navigate = useNavigate();
@@ -20,26 +19,22 @@ const Root: ParentComponent = (props) => {
     });
 
     return (
-        <>
-            <Sprite />
+        <div class="flex flex-col flex-grow">
+            <div class="flex flex-grow">
+                <Playlists />
 
-            <div class="flex flex-col flex-grow">
-                <div class="flex flex-grow">
-                    <Playlists />
-
-                    <Transition name="fade" mode="outin">
-                        {props.children}
-                    </Transition>
-                </div>
-                {/* <Prompt /> */}
-
-                <Player />
-
-                <Menu />
-
-                <Notifications />
+                <Transition name="fade" mode="outin">
+                    {props.children}
+                </Transition>
             </div>
-        </>
+            {/* <Prompt /> */}
+
+            <Player />
+
+            <Menu />
+
+            <Notifications />
+        </div>
     );
 };
 

0dbcdb17293347b1edddbc4bb2207a2714869314
Kirpal <makanga.kirpal@gmail.com>
2024-10-08 20:56:09 +0200

    fetching file metadata (wip)

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,8 +1,94 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use std::fs;
+use std::fs::Metadata;
+use std::path::Path;
+use walkdir::WalkDir;
+use walkdir::DirEntry;
+use serde::{Serialize, Deserialize};
+
 fn main() {
   tauri::Builder::default()
+    .invoke_handler(tauri::generate_handler![get_tracks])
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
+}
+
+fn is_hidden(entry: &DirEntry) -> bool {
+  entry.file_name()
+    .to_str()
+    .map(|s| s.starts_with("."))
+    .unwrap_or(false)
+}
+
+const ALLOWED_FILE_EXTENSIONS: [&str; 8] = ["aiff", "flac", "m4a", "mid", "mp3", "mp4", "ogg", "wav"];
+
+// fn is_allowed_mime_type(path: String) -> bool {
+//   ALLOWED_FILE_EXTENSIONS.contains(Path::new(&path).extension().and_then(OsStr::to_str).unwrap())
+// }
+
+fn get_file_metadata(path: String) -> Metadata {
+  let metadata = match fs::metadata(path) {
+      Ok(data) => data,
+      Err(error) => panic!("Problem fetching metadata: {error:?}")
+  };
+
+  metadata
+}
+
+
+#[derive(Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+struct Track {
+  id: String,
+  title: String,
+  artists: String,
+  album_artists: String,
+  album: String,
+  genre: String,
+  duration: u16,
+  track_number: u16,
+  disc_number: String,
+  source: String,
+  year: u16,
+  cover: String
+}
+
+#[tauri::command]
+fn get_tracks(paths: Vec<String>) -> Vec<Metadata> {
+// fn get_tracks(paths: Vec<String>) -> Vec<Track> {
+  let mut file_paths: Vec<String> = Vec::new();
+  let mut tracks: Vec<Metadata> = Vec::new();
+
+  for path in paths {
+    if Path::new(&path).is_dir() {
+      //TODO: add is_dir(entry) filter properly
+      for entry in WalkDir::new(path).into_iter().filter_entry(|e| !is_hidden(e)) {
+        let file_path = match entry {
+            Ok(entry) => entry.path().to_str().unwrap().to_string(),
+            Err(error) => panic!("Problem opening the file: {error:?}")
+        };
+
+        if Path::new(&file_path).is_file() {
+          file_paths.push(file_path);
+        }
+      }
+    }
+    else {
+        file_paths.push(path);
+    }
+  }
+
+  for file_path in file_paths {
+    tracks.push(get_file_metadata(file_path));
+  }
+
+
+  // keep only files with audio mime types
+
+  // fetch metadata
+
+  // tracks
+  tracks
 }
--- src/components/FileDrop.tsx
+++ src/components/FileDrop.tsx
@@ -1,114 +1,12 @@
+import { invoke } from '@tauri-apps/api';
 import { listen } from '@tauri-apps/api/event';
-import { readDir, BaseDirectory } from '@tauri-apps/api/fs';
-import { appWindow } from '@tauri-apps/api/window';
 import { createSignal, ParentComponent, Show } from 'solid-js';
 import { Transition } from 'solid-transition-group';
-import { parseWebStream } from 'music-metadata';
 
 import Icon from './Icon';
 
 interface Props {
     onDropFiles: (files: Track[]) => void;
-}
-
-async function readEntriesPromise(
-    directoryReader: FileSystemDirectoryReader
-): Promise<FileSystemEntry[]> {
-    return new Promise((resolve, reject) =>
-        directoryReader.readEntries(resolve, reject)
-    );
-}
-
-async function readDirectoryEntries(
-    directoryReader: FileSystemDirectoryReader
-) {
-    let entries = [];
-
-    let readEntries = await readEntriesPromise(directoryReader);
-
-    while (readEntries.length > 0) {
-        entries.push(...readEntries);
-        readEntries = await readEntriesPromise(directoryReader);
-    }
-
-    return entries;
-}
-
-async function getAllFileEntries(dataTransferItemList: DataTransferItemList) {
-    /* TODO: refacto with proper recursion */
-    let fileEntries: FileSystemFileEntry[] = [];
-
-    let queue: FileSystemEntry[] = [];
-
-    for (const item of dataTransferItemList) {
-        const entry = item?.webkitGetAsEntry();
-
-        if (entry) queue.push(entry);
-    }
-
-    while (queue.length > 0) {
-        const entry = queue.shift();
-
-        if (!entry) continue;
-
-        if (entry.isFile) {
-            fileEntries.push(entry as FileSystemFileEntry);
-        } else if (entry.isDirectory) {
-            queue.push(
-                ...(await readDirectoryEntries(
-                    (entry as FileSystemDirectoryEntry).createReader()
-                ))
-            );
-        }
-    }
-
-    return fileEntries;
-}
-
-function getFileFromEntry(entry: FileSystemFileEntry): Promise<File> {
-    return new Promise((resolve, reject) => entry.file(resolve, reject));
-}
-
-async function getFileMetadata(filePath: string): Track {
-    const { body: fileStream } = await fetch(filePath);
-    const {
-        common: {
-            album,
-            albumartist: albumArtists,
-            artist: artists,
-            disk: { no: discNumber, of: discCount },
-            genre,
-            picture: [
-                { data: pictureBuffer, format: pictureFormat } = {
-                    data: null,
-                    format: null
-                }
-            ] = [],
-            title,
-            track: { no: trackNumber },
-            year
-        },
-        format: { duration, codec }
-    } = await parseWebStream(fileStream);
-
-    return {
-        id: uuid(),
-        // title: title || path.parse(filePath).name, // TODO: récupérer le nom du fichier
-        title,
-        artists,
-        albumArtists,
-        album,
-        genre: genre ? genre.join(' ').trim() : '',
-        year,
-        duration,
-        trackNumber,
-        discNumber,
-        discCount,
-        cover: pictureBuffer
-            ? `data:${pictureFormat};base64,${pictureBuffer.toString('base64')}`
-            : '',
-        codec
-    };
 }
 
 const FileDrop: ParentComponent<Props> = (props) => {
@@ -129,14 +27,14 @@ const FileDrop: ParentComponent<Props> = (props) => {
             // );
             unlistenFileDrop = await listen(
                 'tauri://file-drop',
-                async ({ payload }) => {
-                    console.log(payload);
-                    const entries = await readDir('users', {
-                        dir: BaseDirectory.AppData,
-                        recursive: true
-                    });
+                async ({ payload: paths }) => {
+                    console.log(paths);
 
-                    console.log({ entries });
+                    const tracks = await invoke('get_tracks', { paths });
+
+                    console.log({ tracks });
+
+                    // props.onDropFiles(tracks as Track[]);
                 }
             );
         }
@@ -152,50 +50,12 @@ const FileDrop: ParentComponent<Props> = (props) => {
         setIsDraggedOver(false);
     }
 
-    async function handleDrop(e: DragEvent) {
-        e.preventDefault();
-
-        const { items } = e.dataTransfer || {};
-
-        if (!items) return;
-
-        const entries = await getAllFileEntries(items);
-
-        if (entries.length) {
-            /* TODO: Filter unsupported file types */
-            const filteredEntries = [];
-
-            setIsProcessing(true);
-
-            // for (const entry of entries) {
-            //     const { path, type } = await getFileFromEntry(entry);
-
-            //     const source = `file://${path}`;
-
-            //     if (type.startsWith('audio/')) {
-            //         filteredEntries.push({
-            //             ...(await getFileMetadata(source)),
-            //             source
-            //         });
-            //     }
-            // }
-
-            console.log({ filteredEntries });
-
-            setIsProcessing(false);
-
-            props.onDropFiles(filteredEntries);
-        }
-
-        setIsDraggedOver(false);
-    }
-
     return (
         <div
             class="relative flex flex-col flex-grow"
             onDragOver={handleDragOver}
             onDragLeave={handleDragLeave}
-            onDrop={handleDrop}
+            // onDrop={handleDrop}
         >
             {props.children}
 

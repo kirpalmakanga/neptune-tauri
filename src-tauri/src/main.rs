7b0bacbe03d79a9b51debc20ba8f1aae6c22e343
Kirpal <makanga.kirpal@gmail.com>
2024-10-10 17:02:21 +0200

    tauri: removed unused dependency

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -19,16 +19,9 @@ fn main() {
     .expect("error while running tauri application");
 }
 
-fn is_hidden(entry: &DirEntry) -> bool {
-  entry.file_name()
-    .to_str()
-    .map(|s| s.starts_with("."))
-    .unwrap_or(false)
-}
-
 const ALLOWED_FILE_EXTENSIONS: [&str; 6] = ["aiff", "flac", "m4a", "mp3", "mp4", "wav"];
 
-fn is_allowed_mime_type(path: String) -> bool {
+fn is_allowed_mime_type(path: &String) -> bool {
   let extension = Path::new(&path).extension().unwrap().to_str().unwrap();
 
   ALLOWED_FILE_EXTENSIONS.contains(&extension)
@@ -47,6 +40,24 @@ fn encode_album_cover(picture: Picture) -> String {
   let mime_type: String = picture.mime_type.try_into().unwrap();
 
   format!("data:{mime_type};base64,{encoded_image}")
+}
+
+
+#[derive(Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+struct Track {
+  id: String,
+  title: String,
+  album_title: String,
+  artists: String,
+  album_artists: String,
+  genre: String,
+  duration: f64,
+  track_number: u16,
+  disc_number: u16,
+  source: String,
+  year: i32,
+  cover: String
 }
 
 fn get_file_metadata(path: String) -> Track {
@@ -81,34 +92,25 @@ fn get_file_metadata(path: String) -> Track {
   };
 
   track
-  // metadata
 }
 
-
-#[derive(Serialize, Deserialize)]
-#[serde(rename_all = "camelCase")]
-struct Track {
-  id: String,
-  title: String,
-  album_title: String,
-  artists: String,
-  album_artists: String,
-  genre: String,
-  duration: f64,
-  track_number: u16,
-  disc_number: u16,
-  source: String,
-  year: i32,
-  cover: String
-}
-
-#[tauri::command]
-fn get_tracks(paths: Vec<String>) -> Vec<Track> {
-// fn get_tracks(paths: Vec<String>) -> Vec<Track> {
+fn flatten_paths(paths: Vec<String>) -> Vec<String> {
   let mut file_paths: Vec<String> = Vec::new();
-  let mut tracks: Vec<Track> = Vec::new();
 
-  for path in paths {
+  fn is_hidden(entry: &DirEntry) -> bool {
+    entry.file_name()
+      .to_str()
+      .map(|s| s.starts_with("."))
+      .unwrap_or(false)
+  }
+
+  fn add_file_path(path: &String, paths_list: &mut Vec<String>) {
+    if Path::new(path).is_file() && is_allowed_mime_type(path) {
+      paths_list.push(path.to_owned());
+    }
+  }
+
+  for path in paths.into_iter().filter(|p| !Path::new(p).file_name().unwrap().to_str().unwrap_or("").starts_with('.')) {
     if Path::new(&path).is_dir() {
       // TODO: -add is_dir(entry) filter properly
       for entry in WalkDir::new(path).into_iter().filter_entry(|e| !is_hidden(e)) {
@@ -117,25 +119,27 @@ fn get_tracks(paths: Vec<String>) -> Vec<Track> {
             Err(error) => panic!("Problem opening the file: {error:?}")
         };
 
-        if Path::new(&file_path).is_file() {
-          file_paths.push(file_path);
-        }
+        add_file_path(&file_path, &mut file_paths);
       }
     }
     else {
-        file_paths.push(path);
+      add_file_path(&path, &mut file_paths);
     }
   }
+
+  file_paths
+}
+
+#[tauri::command]
+fn get_tracks(paths: Vec<String>) -> Vec<Track> {
+  let mut file_paths: Vec<String> = flatten_paths(paths);
+  let mut tracks: Vec<Track> = Vec::new();
+
+  file_paths.sort();
 
   for file_path in file_paths {
     tracks.push(get_file_metadata(file_path));
   }
 
-
-  // keep only files with audio mime types
-
-  // fetch metadata
-
-  // tracks
   tracks
 }
